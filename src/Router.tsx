615a736eee87785fb4dff904bec56e7a40fdd446
yMarceloMaia <marcelomaiajp@gmail.com>
2025-08-24 14:06:45 -0300

    wip

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
-// import Projects from "./pages/Projects";
+import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Start from "./pages/Start";
@@ -14,10 +14,7 @@ export function Router() {
       <Routes location={location} key={location.pathname}>
         <Route index element={<Start />} />
         <Route path="/home" element={<Home />} />
-        {/* <Route
-                    path="/projects"
-                    element={<Projects />}
-                /> */}
+        <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,37 @@
-// import { useEffect, useState } from "react";
 import { useLanguage } from "../contexts/LanguageContext";
 import Layout from "../components/Layout";
 import "./styles.css";
+import { useTheme } from "../contexts/ThemeContext";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const { language } = useLanguage();
-  // const [content, setContent] = useState({ title: "", subtitle: "" });
-
-  // useEffect(() => {
-  //   const loadContent = async () => {
-  //     const contentData = await import(`../jsons/home-${language}.json`);
-  //     setContent(contentData.default);
-  //   };
-
-  //   loadContent();
-  // }, [language]);
+  const { theme } = useTheme();
+  const navigate = useNavigate();
 
   return (
     <Layout>
-      <main className="z-30">
+      <main className="z-30 flex flex-col items-center justify-center h-full">
         <h1 className="absolute right-0 bottom-0 text-[200px] font-body z-0 opacity-[.02]">
           HOME
         </h1>
-        <section className="mt-32  flex">
-          <p className="md:w-2/4 object-cover p-5 ml-10 flex items-center min-h-40">
+        <section className="w-3/4 text-center">
+          <p className="text-lg">
             {language === "en"
               ? "Hello, my name is Marcelo, and I am a full-stack developer. Always ready for new challenges and creative collaborations. Take a look at my portfolio, and let's create something amazing together!"
               : "Olá, meu nome é Marcelo e sou desenvolvedor full-stack. Sempre pronto para novos desafios e colaborações criativas. Dê uma olhada no meu portfólio e vamos criar algo incrível juntos!"}
           </p>
+        </section>
+
+        <section className="w-3/4 mt-10 text-center flex justify-center">
+          <div className="radio-wrapper" style={{ width: "150px" }}>
+            <button
+              onClick={() => navigate("/contact")}
+              className={`btn ${theme === "light" ? "" : "btn-light"}`}
+            >
+              {language === "en" ? "Contact Me" : "Entre em contato"}
+            </button>
+          </div>
         </section>
       </main>
     </Layout>
